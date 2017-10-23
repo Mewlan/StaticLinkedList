b87e4a558ccd39ff9ff0959c64766f7f957bcfbf
Mewlan <m4wlan@icloud.com>
2017-10-23 14:29:23 +0800

    Update StaticLinkedListModelViewController.c

--- StaticLinkedListModelViewController.c
+++ StaticLinkedListModelViewController.c
@@ -1,15 +1,9 @@
 //
-
 //  StaticLinkedListModelViewController.c
-
 //  StaticLinkedList
-
 //
-
 //  Created by Mewlan Musajan on 10/23/17.
-
 //  Copyright (c) 2017 Mewlan Musajan. All rights reserved.
-
 //
 
 #include <stdio.h>
@@ -21,7 +15,7 @@ struct Student
 	struct Student *next;
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
 	model();
 	return 0;
@@ -42,26 +36,24 @@ void view(struct Student *ptr)
 {
 	printf("Enter student number, score:\n");
 	inPutController(ptr);
-	outPutViewController(ptr);
+	outPutController(ptr);
 }
 
 void inPutController(struct Student *ptr)
 {
 	struct Student *original;
-	original = ptr;
-	do 
+	do
 	{
-		scanf("%d, %f", &ptr->number, &ptr->score);
+		scanf("%u, %f", &ptr->number, &ptr->score);
 		ptr = ptr->next;
 	} while(ptr != NULL);
-	ptr = original;
 }
 
-void outPutViewController(struct Student *ptr)
+void outPutController(struct Student *ptr)
 {
 	do
 	{
-		printf("Student number: %d, score: %0.2f\n", ptr->number, ptr->score);
+		printf("Student number: %u, score: %0.2f\n", ptr->number, ptr->score);
 		ptr = ptr->next;
 	} while(ptr != NULL);
 }
